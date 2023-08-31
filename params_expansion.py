a9441ec3c87bda61fe807169a06d036f85bf0972
Google Jr <andilembele020@gmail.com>
2023-08-31 22:06:30 +0200

    Updated parameter expansion with doctests

--- params_expansion.py
+++ params_expansion.py
@@ -1,25 +1,50 @@
-'''
-@author: Google Jr
-@program: parameter expansion
-'''
-
 class ParameterExpansion:
     def example1(self):
+        '''
+        >>> pe = ParameterExpansion()
+        >>> pe.example1()
+        6
+        15
+        '''
+
         # old way
         def add(a, b, c):
+            '''
+            >>> add(1, 2, 3)
+            6
+            >>> add(3, 5, 7)
+            15
+            '''
             return a + b + c
-
-        print(add(1, 2, 3))
 
         # new way
         def add(*args):
+            '''
+            >>> add(1, 2, 3)
+            6
+            >>> add(3, 5, 7)
+            15
+            '''
             return sum(args)
 
         print(add(1, 2, 3))
+        print(add(3, 5, 7))
 
     def example2(self):
+        '''
+        >>> pe = ParameterExpansion()
+        >>> pe.example2()
+        6
+        12
+        '''
+
         # old way
         def add(a, b, c):
+            '''
+            >>> numbers = [1, 2, 3]
+            >>> add(numbers[0], numbers[1], numbers[2])
+            6
+            '''
             return a + b + c
 
         numbers = [1, 2, 3]
@@ -27,14 +52,20 @@ class ParameterExpansion:
 
         # new way
         def add(*args):
+            '''
+            >>> numbers = [6, 2, 4]
+            >>> add(*numbers)
+            12
+            '''
             return sum(args)
 
-        numbers = [1, 2, 3]
+        numbers = [6, 2, 4]
         print(add(*numbers))
 
-    
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     pe = ParameterExpansion()
     print("Example 1:")
     pe.example1()
