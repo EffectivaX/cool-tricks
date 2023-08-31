a7edce33651e9d520972c51afeeb8d95f8fe30b2
Google Jr <andilembele020@gmail.com>
2023-08-31 21:39:19 +0200

    params expansion with list and dicts

---
+++ params_expansion_2.py
@@ -0,0 +1,31 @@
+'''
+@author: Google Jr
+@program: parameter expansion
+'''
+
+class ParameterExpansion:
+    # old way
+    def process_student_info(self, name, last_name, subject, score):
+        print(f"{name} {last_name} scored {score} in {subject}")
+    
+    # new way
+    def process_student_info__using_args(self, *args):
+        if len(args) == 4:
+            name, last_name, subject, score = args
+            print(f"{name} {last_name} scored {score} in {subject}")
+        elif len(args) == 3:
+            name, last_name, details = args
+            print(f"{name} {last_name} scored {details['score']} in {details['subject']}")
+
+
+
+if __name__ == "__main__":
+    pe = ParameterExpansion()
+    # Example 1: Using individual arguments
+    print("Example 1:")
+    pe.process_student_info("Andile", "Mbele", "Computational Thinking", 100)
+
+    # Example 2: Using a dictionary
+    print("Example 2:")
+    student_details = {"name": "Andile", "last_name": "Mbele", "subject": "Computational Thinking", "score": 100}
+    pe.process_student_info(**student_details)
