bd79200a3d4f10e356057f02413c34e01c003dfd
Google Jr <andilembele020@gmail.com>
2023-08-31 19:34:06 +0200

    OOP implemented

--- list_compression.py
+++ list_compression.py
@@ -3,34 +3,44 @@
 @program: list compression
 '''
 
-# list compression
-# list compression is a way to create a list using a single line of code
+class ListCompressor:
+    def example1(self):
+        numbers = [i ** 2 for i in range(0, 11)]
+        return numbers
 
-# example 1
-numbers = [i ** 2 for i in range(0, 11)]
-print(numbers) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    def example2(self):
+        names = ["Andile", "Mzie", "Ayanda", "Zola", "Olwethu", "Nondyebo"]
+        first_letters = [name[0] for name in names]
+        return first_letters
 
-# example 2
-names = ["Andile", "Mzie", "Ayanda", "Zola", "Olwethu", "Nondyebo"]
-first_letters = [name[0] for name in names]
-print(first_letters) # ['A', 'M', 'A', 'Z', 'O', 'N']
+    def example3(self):
+        numbers = [i * 2 for i in range(0, 11) if i % 2 == 0]
+        return numbers
 
-# example 3
-numbers = [i * 2 for i in range(0, 11) if i % 2 == 0]
-print(numbers) # [0, 4, 8, 12, 16, 20]
+    def example4(self):
+        numbers = [i * 2 if i % 2 == 0 else i * 3 for i in range(0, 11)]
+        return numbers
 
-# example 4
-numbers = [i * 2 if i % 2 == 0 else i * 3 for i in range(0, 11)]
-print(numbers) # [0, 3, 4, 9, 8, 15, 12, 21, 16, 27, 20]
+    def example5(self):
+        numbers = [i * 2 if i % 2 == 0 else i * 3 for i in range(0, 11) if i % 3 == 0]
+        return numbers
 
-# example 5
-numbers = [i * 2 if i % 2 == 0 else i * 3 for i in range(0, 11) if i % 3 == 0]
-print(numbers) # [0, 9, 12, 27]
+    def example6(self):
+        numbers = [i * 2 if i % 2 == 0 else i * 3 for i in range(0, 11) if i % 3 == 0 and i % 2 == 0]
+        return numbers
 
-# example 6
-numbers = [i * 2 if i % 2 == 0 else i * 3 for i in range(0, 11) if i % 3 == 0 and i % 2 == 0]
-print(numbers) # [0, 12]
+    def example7(self):
+        numbers = [i for i in range(1, 101) if i % 12 == 0]
+        return numbers
 
-# example 7: More complex list compression
-numbers = [i for i in range(1, 101) if i % 12 == 0]
-print(numbers) # [12, 24, 36, 48, 60, 72, 84, 96]
+
+if __name__ == "__main__":
+    lc = ListCompressor()
+    print("Example 1:", lc.example1()) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    print("Example 2:", lc.example2()) # ['A', 'M', 'A', 'Z', 'O', 'N']
+    print("Example 3:", lc.example3()) # [0, 4, 8, 12, 16, 20]
+    print("Example 4:", lc.example4()) # [0, 3, 4, 9, 8, 15, 12, 21, 16, 27, 20]
+    print("Example 5:", lc.example5()) # [0, 9, 12, 21, 24, 33, 36, 45, 48, 57, 60]
+    print("Example 6:", lc.example6()) # [0, 12, 24, 36, 48, 60]
+    print("Example 7:", lc.example7()) # [12, 24, 36, 48, 60, 72, 84, 96]
+
